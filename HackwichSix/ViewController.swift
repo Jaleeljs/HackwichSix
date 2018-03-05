20f6845ce00d9ae01b53c313d29c14ca02f1c1a8
Jaleeljs <35237438+Jaleeljs@users.noreply.github.com>
2018-03-05 10:47:24 -1000

    HackwichSix completed

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -2,24 +2,49 @@
 //  ViewController.swift
 //  HackwichSix
 //
-//  Created by Xanda-Li Kinney-Kawai on 3/5/18.
+//  Created by JaleelShavers on 3/5/18.
 //  Copyright © 2018 jaleeljs. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var slider: UISlider!
+    //create a variable that can hold the value of the slider
+    var currentValue: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func sliderHasMoved(_ sender: Any) {
+        
+        print("The value of the slider is:\(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        //5. New variable message that displays a message that includes currentValue
+        let message = "The value is: \(currentValue)"
+        
+        //1. create alert view
+        let alert = UIAlertController(title:"Hello World!", message: message, preferredStyle: .alert)
+        
+        //2. button that user taps to dismiss view controller
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        //3. add the button to the alertview
+        alert.addAction(action)
+        
+        //4. present alertview on the screen
+        present(alert, animated: true, completion: nil)
 }
 
+}
